97987c2ae72e469d5dd2ebd3621e6217a6b76f62
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-10-19 15:52:37 +0700

    feat: structure teamplate admin && handle export data Excel file button

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import "./styles/_all.scss";
 import { lazy, Suspense } from "react";
 import { Provider } from "react-redux";
 import { FloatButton } from "antd";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { unstable_HistoryRouter as HistoryRouter, Route, Routes } from 'react-router-dom'
+import { createBrowserHistory } from "history";
 import ReactDOM from "react-dom/client";
-import AdminTemplate from "./templates/AdminTemplate";
 import Loading from "./components/loading";
 import store from "./redux/store";
 
+const AdminTemplate = lazy(() => import("./templates/AdminTemplate"));
+const ExcelTemplate = lazy(() => import("./templates/ExportExcel"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage"));
+
+export const routeLink: any = createBrowserHistory();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -16,13 +20,14 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={store}>
     <Suspense fallback={<Loading />}>
-      <BrowserRouter>
+    <HistoryRouter history={routeLink}>
         <Routes>
           <Route path="" element={<AdminTemplate />}></Route>
+          <Route path="/excel" element={<ExcelTemplate />}></Route>
 
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </BrowserRouter>
+      </HistoryRouter>
     </Suspense>
     <FloatButton.BackTop tooltip={<div>Back to top</div>} />
   </Provider>
--- src/templates/AdminTemplate.tsx
+++ src/templates/AdminTemplate.tsx
@@ -1,82 +1,219 @@
-import React from 'react';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import {
+   MenuFoldOutlined,
+   MenuUnfoldOutlined,
+   UserOutlined,
+} from '@ant-design/icons';
+
+import logo from "../assets/images/logo-company.png";
+
+import { Button, Layout, Menu, theme } from 'antd';
 import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Link } from "react-router-dom";
 
-const { Header, Content, Sider } = Layout;
+import { PiShareNetworkBold } from 'react-icons/pi';
+import { MdOutlineWorkOutline } from 'react-icons/md';
+import { VscGraph, VscGroupByRefType } from 'react-icons/vsc';
 
-const items1: MenuProps['items'] = ['1', '2', '3'].map((key) => ({
-  key,
-  label: `nav ${key}`,
-}));
 
-const items2: MenuProps['items'] = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
 
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `subnav ${key}`,
+import TableData from '../components/table';
 
-      children: new Array(4).fill(null).map((_, j) => {
-        const subKey = index * 4 + j + 1;
-        return {
-          key: subKey,
-          label: `option${subKey}`,
-        };
-      }),
-    };
-  },
-);
 
-const AdminTemplate: React.FC = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
 
-  return (
-    <Layout>
-      <Header style={{ display: 'flex', alignItems: 'center' }}>
-        <div className="demo-logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={items1}
-          style={{ flex: 1, minWidth: 0 }}
-        />
-      </Header>
+const { Header, Sider, Content, Footer } = Layout;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+   label: React.ReactNode,
+   key: React.Key,
+   icon?: React.ReactNode,
+   children?: MenuItem[],
+): MenuItem {
+   return {
+      key,
+      icon,
+      children,
+      label,
+   } as MenuItem;
+}
+
+const items: MenuItem[] = [
+   getItem('Quản lý tài khoản', '1', <UserOutlined />),
+   getItem('Quản lý việc làm', '2', <PiShareNetworkBold />),
+   getItem('Quản lý loại việc làm', 'sub1', <MdOutlineWorkOutline />, [
+      getItem('Tom', '3'),
+      getItem('Bill', '4'),
+      getItem('Alex', '5'),
+   ]),
+   getItem('Quản lý skill', 'sub2', <VscGroupByRefType />, [
+      getItem('Team 1', '6'),
+      getItem('Team 2', '7')
+   ]),
+   getItem('Thống kê', '8', <VscGraph />),
+
+];
+
+type Props = {}
+
+
+const TestTemplate = (props: Props) => {
+   const [collapsed, setCollapsed] = useState(false);
+   const {
+      token: { colorBgContainer, borderRadiusLG },
+   } = theme.useToken();
+
+   return (
       <Layout>
-        <Sider width={200} style={{ background: colorBgContainer }}>
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0 }}
-            items={items2}
-          />
-        </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Breadcrumb
-            items={[{ title: 'Home' }, { title: 'List' }, { title: 'App' }]}
-            style={{ margin: '16px 0' }}
-          />
-          <Content
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
-      </Layout>
-    </Layout>
-  );
-};
 
-export default AdminTemplate;
+         {/* <Button
+            style={{
+               marginBottom: 20
+            }}
+            onClick={() => {
+               const excel = new Excel();
+               excel
+                  .addSheet("test")
+                  .addColumns(columns)
+                  .addDataSource(data)
+                  .saveAs("测试.xlsx");
+            }}
+         >
+            下载
+         </Button> */}
+
+         <Header style={{ display: 'flex', alignItems: 'center' }}>
+            <Link to="/" className="flex justify-center items-center gap-2">
+               <img src={logo} alt="" className="w-10 h-10" loading="lazy" />
+               <h1 className="lg:text-[28px]
+                              md:text-[20px]
+                              w-60 h-full 
+                              text-white 
+                              font-bold"
+               >EasyJob</h1>
+            </Link>
+
+            {/* THÊM CODE TÊN ADMIN + TÊN Ở DƯỚI ĐÂY */}
+
+         </Header>
+
+         <Layout>
+
+
+            <Sider
+               trigger={null}
+               collapsible collapsed={collapsed}
+               style={{ height: 'full' }}
+               width={220}
+            >
+
+               <div className="demo-logo-vertical" />
+               <Menu
+                  theme="dark"
+                  mode="inline"
+                  defaultSelectedKeys={['1']}
+                  // style={{width: 500}}
+
+                  // CÁCH VIẾT 1: ⭐⭐⭐⭐⭐
+                  items={items}
+
+               // CÁCH VIẾT 2: ⭐⭐⭐⭐⭐
+               // items={[
+               //    {
+               //       key: '1',
+               //       icon: <UserOutlined />,
+               //       label: 'Quản lý tài khoản',
+               //       children: [
+               //          {
+               //             key: '1-1',
+               //             label: 'Danh sách tài khoản',
+               //          },
+               //          {
+               //             key: '1-2',
+               //             label: 'Thêm tài khoản mới',
+               //          },
+               //       ],
+               //    },
+               //    {
+               //       key: '2',
+               //       icon: <PiShareNetworkBold />,
+               //       label: 'Quản lý việc làm',
+               //    },
+               //    {
+               //       key: '3',
+               //       icon: <MdOutlineWorkOutline />,
+               //       label: 'Quản lý loại việc làm',
+               //    },
+               //    {
+               //       key: '4',
+               //       icon: <VscGroupByRefType />,
+               //       label: 'Quản lý skill',
+               //    },
+               //    {
+               //       key: '5',
+               //       icon: <VscGraph />,
+               //       label: 'Thống kê',
+               //    },
+               // ]}
+               />
+            </Sider>
+
+
+
+
+            <Layout>
+
+               <Header style={{ padding: 0, background: colorBgContainer }}>
+
+                  {/* BUTTON THU GỌN MENU */}
+                  <Button
+                     type="text"
+                     icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                     onClick={() => setCollapsed(!collapsed)}
+                     style={{
+                        fontSize: '16px',
+                        width: 64,
+                        height: 64,
+                        background: 'red'       // ############⭐⭐⭐⭐⭐   
+                     }}
+                  />
+               </Header>
+
+
+               <Content
+                  style={{
+                     padding: 12,
+                     margin: '24px 16px',
+                     minHeight: 280,
+                     background: colorBgContainer,
+                     borderRadius: borderRadiusLG,
+                     // backgroundColor: 'pink'    // ############⭐⭐⭐⭐⭐   
+                  }}
+               >
+                  {/* CONTENT */}
+                  <TableData />
+
+
+               </Content>
+
+
+
+
+               <Footer style={{ textAlign: 'center' }}>
+                  EasyJob ©{new Date().getFullYear()}
+               </Footer>
+
+            </Layout>
+
+
+
+
+
+
+         </Layout>
+      </Layout>
+   );
+}
+
+export default TestTemplate;
