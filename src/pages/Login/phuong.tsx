2f89286a6d2d4f8b9732c4accbae470909a0e144
Nguyen Duc Tan <nguyenductan998@gmail.com>
2024-10-21 10:10:02 +0700

    feat: pull code

--- src/pages/Login/phuong.tsx
+++ src/pages/Login/phuong.tsx
@@ -1,30 +1,35 @@
 import React, { useEffect, useRef, useState } from "react";
-import styles from "./index.module.css";
 // import 'bootstrap-icons/font/bootstrap-icons.css';
+import logo from "../../assets/images/logo-company.png";
 
 const IndexPage: React.FC = () => {
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState<boolean>(false);
+
+  const toggleActive = () => {
+    setActive(!active);
+  };
 
   return (
     <div className={`flex justify-center items-center min-h-screen w-full`}>
       <div
-        className={`relative w-3/5 h-[650px] shadow-lg rounded-md overflow-hidden flex ${
+         className={`relative w-3/5 h-[650px] shadow-[rgba(0,0,0,0.25)_0px_14px_28px,rgba(0,0,0,0.22)_0px_10px_10px] rounded-3xl overflow-hidden flex ${
           active ? "transform transition-all duration-[1.5s]" : ""
         }`}
       >
         {/* Sign In/Up Forms */}
         <div
-          className={`relative w-1/2 flex flex-col items-center justify-center text-center bg-white transition-transform duration-[1.15s] ${
+           className={`relative w-1/2 flex flex-col items-center justify-center text-center bg-white transition-transform duration-[1.15s] ${
             active ? "translate-x-full" : ""
           }`}
         >
           {/* Sign In Form */}
           <div
-            className={`absolute w-full h-full flex flex-col items-center justify-center text-center ${
+             className={`absolute w-full h-full flex flex-col items-center justify-center text-center transition-opacity duration-1000 ${
               active ? "opacity-0 z-0" : "opacity-100 z-50"
             }`}
           >
-            <h3 className="text-[40px] font-bold text-[#222]">Sign In</h3>
+            <img src={logo} alt="img-logo" />
+            <h3 className="text-4xl text-gray-800">Sign In</h3>
             <span className="text-gray-600">or use your account</span>
             <form className="mt-6">
               <div className="mb-6">
@@ -57,10 +62,11 @@ const IndexPage: React.FC = () => {
 
           {/* Sign Up Form */}
           <div
-            className={`absolute w-full h-full flex flex-col items-center justify-center text-center ${
+             className={`absolute w-full h-full flex flex-col items-center justify-center text-center transition-opacity duration-1000 ${
               active ? "opacity-100 z-50" : "opacity-0 z-0"
             }`}
           >
+            <img src={logo} alt="img-logo" />
             <h3 className="text-4xl text-gray-800">Sign Up</h3>
             <span className="text-gray-600">
               or use your email for registration
@@ -100,13 +106,13 @@ const IndexPage: React.FC = () => {
         {/* Overlay */}
         <div
           className={`w-1/2 bg-[#3e8fff] flex flex-col items-center justify-center text-center transition-transform duration-[1.5s] ${
-            active ? "-translate-x-full" : ""
+            active ? "-translate-x-full" : "translate-x-0"
           }`}
         >
           {/* Sign In Side */}
           <div
-            className={`absolute flex flex-col items-center justify-center h-full w-full ${
-              active ? "opacity-0 -ml-full" : "opacity-100"
+            className={`absolute flex flex-col items-center justify-center gap-5 h-full w-[450px] transition-transform duration-1000 ${
+              active ? "-translate-x-full" : "translate-x-0"
             }`}
           >
             <h3 className="text-4xl text-white">Welcome Back!</h3>
@@ -114,7 +120,7 @@ const IndexPage: React.FC = () => {
               To keep connected with us please login with your personal info
             </p>
             <button
-              onClick={() => setActive(false)}
+              onClick={toggleActive}
               className="text-white py-3 px-12 border border-white rounded-full"
             >
               Sign Up <i className="bi bi-arrow-right ml-2"></i>
@@ -123,7 +129,7 @@ const IndexPage: React.FC = () => {
 
           {/* Sign Up Side */}
           <div
-            className={`absolute flex flex-col items-center justify-center h-full w-full ${
+            className={`absolute flex flex-col items-center justify-center gap-5 h-full w-[430px] transition-opacity duration-1000 ${
               active ? "opacity-100 ml-0" : "opacity-0"
             }`}
           >
@@ -132,7 +138,7 @@ const IndexPage: React.FC = () => {
               Enter your personal details and start your journey with us
             </p>
             <button
-              onClick={() => setActive(true)}
+              onClick={toggleActive}
               className="text-white py-3 px-12 border border-white rounded-full"
             >
               <i className="bi bi-arrow-left mr-2"></i> Sign In
@@ -142,6 +148,7 @@ const IndexPage: React.FC = () => {
       </div>
     </div>
   );
+  
 };
 
 export default IndexPage;
